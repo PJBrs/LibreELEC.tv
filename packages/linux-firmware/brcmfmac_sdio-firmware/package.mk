42ea864c4311a8299470d140855d7d966c79e0a8
PJBrs <pjbrs@floorenpj.xs4all.nl>
2020-03-29 19:34:00 +0000

    Switch to my own branch of the brcmfmac_sdio-firmware, because I added the BPI-M2-Zero files for bluetooth and nvram
    DO NOT MERGE

--- packages/linux-firmware/brcmfmac_sdio-firmware/package.mk
+++ packages/linux-firmware/brcmfmac_sdio-firmware/package.mk
@@ -2,11 +2,15 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="brcmfmac_sdio-firmware"
-PKG_VERSION="428ee70f59671a5c620466e8be1d320a66c1bf8b"
-PKG_SHA256="6b61755d8735053d00c67c158af4d931d88fd7c4c18413830309e92c44f4b295"
+#PKG_VERSION="428ee70f59671a5c620466e8be1d320a66c1bf8b"
+PKG_VERSION="a263daa9878a1106e1d5914b5f8193169c708641"
+#PKG_SHA256="6b61755d8735053d00c67c158af4d931d88fd7c4c18413830309e92c44f4b295"
+PKG_SHA256="6a4295d8c5dc31098fc5e4c00b43f73c4c9c383b1fb68133a86429c71e44b4a6"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/LibreELEC/brcmfmac_sdio-firmware"
-PKG_URL="https://github.com/LibreELEC/brcmfmac_sdio-firmware/archive/$PKG_VERSION.tar.gz"
+#PKG_SITE="https://github.com/LibreELEC/brcmfmac_sdio-firmware"
+#PKG_URL="https://github.com/LibreELEC/brcmfmac_sdio-firmware/archive/$PKG_VERSION.tar.gz"
+PKG_SITE="https://github.com/PJBrs/brcmfmac_sdio-firmware"
+PKG_URL="https://github.com/PJBrs/brcmfmac_sdio-firmware/archive/$PKG_VERSION.tar.gz"
 PKG_LONGDESC="Broadcom SDIO firmware used with LibreELEC"
 PKG_TOOLCHAIN="manual"
 
